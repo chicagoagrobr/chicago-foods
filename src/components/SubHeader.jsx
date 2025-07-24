e1e6f1190bb9fe14e9d45e791d832f5c5f4e6b5e
sophiasussa <sophiasussa@outlook.com>
2025-07-24 11:16:15 -0300

    Style: centralizar links no subheader e ajustar sombra

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 export default function SubHeader() {
 
     return (
-        <nav className="bg-amber-900 flex items-center justify-between p-4 flex gap-6">
+        <nav className="bg-amber-900 flex items-center justify-center gap-x-12 p-3"
+            style={{
+                boxShadow: `
+                inset 0 4px 6px rgba(0, 0, 0, 0.3),
+                inset 0 -4px 4px rgba(0, 0, 0, 0.2)
+                `
+            }}
+        >
             <Link to="/" className="text-orange-200">Home</Link>
             <Link to="/produto" className="text-orange-200">Produto</Link>
             <Link to="/contato" className="text-orange-200">Contato</Link>
