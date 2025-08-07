7fafc0088f818eafe09b8bbd85d2d19bbaa41172
sophiasussa <sophiasussa@outlook.com>
2025-08-07 14:56:46 -0300

    Feat: avançar com a página de produtos

--- src/pages/ProductsContent.jsx
+++ src/pages/ProductsContent.jsx
@@ -1,3 +1,14 @@
+import germen from '../assets/germen.jpeg';
+import gritz3 from '../assets/gritz3.jpeg';
+import mixpro from '../assets/mixpro.jpeg';
+import fubafino from '../assets/fubafino.jpeg';
+import fubaitaliano from '../assets/fubaitaliano.jpeg';
+import fubamedio from '../assets/fubamedio.jpeg';
+import canjicao from '../assets/canjicao.jpeg';
+import canjiquinha from '../assets/canjiquinha.jpeg';
+import dz1 from '../assets/dz1.jpeg';
+import cf from '../assets/cf.png';
+
 export default function ProductsPage() {
   const products = [
     {
@@ -5,7 +16,7 @@ export default function ProductsPage() {
       name: "GRITZ 1",
       description: "Produto de granulometria fina, ideal para processos industriais que exigem uniformidade e boa fluidez.",
       desc: "Granulação Fina",
-      image: "/assets/molho.jpg",
+      image: "/assets/picles.jpg",
     },
     {
       id: 2,
@@ -15,67 +26,107 @@ export default function ProductsPage() {
       image: "/assets/picles.jpg",
     },
     {
-      id: 3,
-      name: "GRITZ 3",
+      id: 4,
+      name: "MixPro",
       description: "Partículas maiores, ideal para formulações que demandam retenção de estrutura e menor solubilidade imediata.",
       desc: "Granulação Grossa",
-      image: "/assets/ketchup.jpg",
-    },
-    {
-      id: 4,
-      name: "CANJIQUINHA",
-      description: "Produto robusto e tradicional com textura ideal para preparo de alimentos prontos.",
-      desc: "Textura Encorpada",
-      image: "/assets/mostarda.jpg",
+      image: mixpro,
     },
     {
       id: 5,
-      name: "CANJICA",
-      description: "Grão grande ideal para preparações doces e pratos gourmet industrializados.",
-      desc: "Grão Degerminado",
-      image: "/assets/chutney.jpg",
+      name: "GRITZ 3",
+      description: "Partículas maiores, ideal para formulações que demandam retenção de estrutura e menor solubilidade imediata.",
+      desc: "Granulação Grossa",
+      image: gritz3,
     },
     {
       id: 6,
-      name: "FUBÁ FINO",
-      description: "Produto com alta dispersão e absorção, ideal para formulações fermentadas e panificação.",
-      desc: "Moagem Ultrafina",
-      image: "/assets/maionese.jpg",
+      name: "CANJIQUINHA",
+      description: "Produto robusto e tradicional com textura ideal para preparo de alimentos prontos.",
+      desc: "Textura Encorpada",
+      image: canjiquinha,
     },
     {
       id: 7,
+      name: "CANJICA",
+      description: "Grão grande ideal para preparações doces e pratos gourmet industrializados.",
+      desc: "Grão Degerminado",
+      image: dz1,
+    },
+    {
+      id: 8,
+      name: "CANJICAO",
+      description: "Grão grande ideal para preparações doces e pratos gourmet industrializados.",
+      desc: "Grão Degerminado",
+      image: canjicao,
+    },
+    {
+      id: 9,
+      name: "FUBÁ FINO",
+      description: "Produto com alta dispersão e absorção, ideal para formulações fermentadas e panificação.",
+      desc: "Moagem Ultrafina",
+      image: fubafino,
+    },
+    {
+      id: 10,
+      name: "FUBÁ MEDIO",
+      description: "Textura encorpada com toque gourmet, para receitas tradicionais e também processos industriais modernos.",
+      desc: "Moagem Média",
+      image: fubamedio,
+    },
+    {
+      id: 11,
       name: "FUBÁ ITALIANO",
       description: "Textura encorpada com toque gourmet, para receitas tradicionais e também processos industriais modernos.",
       desc: "Moagem Média",
-      image: "/assets/maionese.jpg",
+      image: fubaitaliano,
+    },
+    {
+      id: 11,
+      name: "GERMEN",
+      description: "Textura encorpada com toque gourmet, para receitas tradicionais e também processos industriais modernos.",
+      desc: "Moagem Média",
+      image: germen,
     },
   ];
 
   return (
-    <section className="min-h-screen px-6 py-16 text-gray-800" style={{ boxShadow:`inset 0px -4px 4px rgba(0,0,0,0.10)`}}>
-      <div className="max-w-6xl mx-auto">
-        <h2 className="text-lg font-bold mb-10 text-center text-green-800">
-            A Chicago Foods oferece ao mercado industrial uma linha completa de produtos à base de milho amarelo, produzidos com
-            alto padrão de qualidade, controle de umidade e uniformidade granulométrica. Nossos derivados atendem amplamente às
-            demandas das indústrias alimentícia, cervejeira, pet food, ração animal, entre outras aplicações que exigem padronização,
-            segurança alimentar e estabilidade de fornecimento.
-        </h2>
+  <section className="relative min-h-screen px-6 py-12 text-gray-800 overflow-hidden" style={{ boxShadow:`inset 0px 4px 12px rgba(0,0,0,0.4), inset 0px -4px 8px rgba(0,0,0,0.2)`}}>
+    <div
+      className="absolute inset-0 z-0 top-[100px]"
+      style={{
+        backgroundImage: `url(${cf})`,
+        backgroundSize: '93%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top',
+        filter: 'blur(6px)',
+        opacity: 0.3
+      }}
+    />
 
-        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 pt-10">
-          {products.map((product) => (
-            <div key={product.id} className="bg-white rounded-xl shadow hover:shadow-lg transition p-5 flex flex-col">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="rounded-xl h-48 object-cover mb-4"
-              />
-              <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-              <p className="text-gray-600 mb-4 flex-grow">{product.description}</p>
-              <div className="text-green-800 font-bold text-lg">{product.desc}</div>
-            </div>
-          ))}
-        </div>
+    <div className="relative z-10 max-w-6xl mx-auto">
+      <h2 className="text-lg text-center">
+        A <span className="text-orange-400 font-medium">Chicago Foods</span> oferece ao mercado industrial uma linha completa de produtos à base de milho amarelo, produzidos com
+        alto padrão de qualidade, controle de umidade e uniformidade granulométrica. Nossos derivados atendem amplamente às
+        demandas das indústrias alimentícia, cervejeira, pet food, ração animal, entre outras aplicações que exigem padronização,
+        segurança alimentar e estabilidade de fornecimento.
+      </h2>
+
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4 pt-10">
+        {products.map((product) => (
+          <div key={product.id} className="bg-white bg-opacity-90 rounded-xl shadow-2xl transition-all duration-300 hover:scale-110 p-5 flex flex-col">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="rounded-xl h-48 object-cover mb-4 shadow"
+            />
+            <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
+            <p className="text-gray-600 mb-4 flex-grow">{product.description}</p>
+            <div className="text-green-800 font-bold text-lg">{product.desc}</div>
+          </div>
+        ))}
       </div>
-    </section>
+    </div>
+  </section>
   );
 }
