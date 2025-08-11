a61a7a4821215f4caddc4bc3a27ec8fd603d3751
sophiasussa <sophiasussa@outlook.com>
2025-08-11 15:05:51 -0300

    Feat: funcionalidade para quando mudar de página, ela começar no inicio

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,10 +7,12 @@ import HomeContent from '../pages/HomeContent'
 import ProductsContent from '../pages/ProductsContent'
 import Parceiros from '../components/Parceiros'
 import ProductDetail from '../pages/ProductDetail'
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function AppRoutes() {
   return (
     <div className="flex flex-col min-h-screen bg-lime-50">
+      <ScrollToTop />
       <SubHeader />
       <Header />
       <main className="flex-grow">
