70ed4cb7553bb4aea6c243f7b779f9bf4fe924c2
sophiasussa <sophiasussa@outlook.com>
2025-09-02 14:55:13 -0300

    Feat: logo agora é um link para o home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,10 @@ export default function Header() {
     <header className="bg-green-800 py-3.5 sticky top-0 z-50 shadow-md shadow-green-900/30">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6">
         
-        <div className="flex items-center space-x-1 transition-transform duration-300 hover:scale-105">
+        <Link to="/" className="flex items-center space-x-1 transition-transform duration-300 hover:scale-105">
           <img src={logo} alt="Logo da empresa: Chicago Foods" className="w-20 sm:w-[104px]" />
           <img src={nome} alt="Nome da empresa: Chicago Foods" className="h-10 sm:h-[59px] mt-1" />
-        </div>
+        </Link>
 
         <nav className="hidden md:flex items-center justify-center gap-x-8 p-3">
           <Link to="/" className="flex items-center gap-2 text-orange-100 text-sm hover:text-orange-300 px-2">
