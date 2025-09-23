638ad97eba671b9007e904d2da95026ee456c9a1
sophiasussa <sophiasussa@outlook.com>
2025-09-23 15:29:39 -0300

    style(header): change text color mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ export default function Header() {
               to="/contato"
               onClick={() => {window.scrollTo({ top: 290, behavior: "smooth" }); 
               setMenuOpen(false);}}
-              className="flex items-center bg-orange-400 hover:bg-orange-300 w-44 text-green-900 font-semibold px-4 py-2 font-semibold  rounded-full shadow-2xl transition">
+              className="flex items-center bg-orange-400 hover:bg-orange-300 w-44 text-white font-semibold px-4 py-2 font-semibold  rounded-full shadow-2xl transition">
               Solicitar Orçamento
             </Link>
           </nav>
