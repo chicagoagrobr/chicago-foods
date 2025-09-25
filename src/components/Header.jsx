b788d5f606a24f368e0ecb52b9cf678f76670466
sophiasussa <sophiasussa@outlook.com>
2025-09-25 08:02:00 -0300

    feat(header): enhance responsive design and update styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import nome from '../assets/nome.avif';
 import logo from '../assets/cf.avif';
 import { Link, useLocation } from "react-router-dom";
-import { Home, Boxes, Mail, Menu, X } from "lucide-react";
+import { Home, Boxes, Mail, Menu, X, BookOpen } from "lucide-react";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,15 +22,15 @@ export default function Header() {
           <img src={nome} alt="Nome da empresa: Chicago Foods" className="h-10 sm:h-[59px] mt-1" />
         </Link>
 
-        <nav className="hidden medium:flex items-center justify-center gap-x-8 p-3">
+        <nav className="hidden lg:flex items-center justify-center gap-x-8 p-3">
           <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className={`flex items-center gap-2 ${isActive("/") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} text-sm px-2 font-semibold`}>
             <Home className="w-5 h-5" /> Home
           </Link>
+          <Link to="/sobre" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className={`flex items-center gap-2 ${isActive("/sobre") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} text-sm font-semibold`}>
+            <BookOpen className="w-5 h-5" /> Sobre Nós
+          </Link>
           <Link to="/produtos" className={`flex items-center gap-2 ${isActive("/produtos") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} text-sm font-semibold`}>
             <Boxes className="w-5 h-5" /> Produtos
-          </Link>
-          <Link to="/sobre" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className={`flex items-center gap-2 ${isActive("/sobre") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} text-sm ml-1 font-semibold`}>
-            <Mail className="w-5 h-5" /> Sobre
           </Link>
           <Link to="/contato" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className={`flex items-center gap-2 ${isActive("/contato") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} text-sm ml-1 font-semibold`}>
             <Mail className="w-5 h-5" /> Contato
@@ -38,13 +38,13 @@ export default function Header() {
           <Link
             to="/contato"
             onClick={() => window.scrollTo({ top: 290, behavior: "smooth" })}
-            className="hidden medium:inline-flex items-center bg-orange-400 shadow-2xl hover:bg-orange-300 text-white font-medium px-4 py-2 rounded-full transition">
+            className="hidden lg:inline-flex items-center bg-orange-400 shadow-2xl hover:bg-orange-300 text-white font-medium px-4 py-2 rounded-full transition">
             Solicitar Orçamento
           </Link>
         </nav>
 
         <button
-          className="medium:hidden text-orange-100 hover:text-orange-300 transition-all duration-200"
+          className="lg:hidden text-orange-100 hover:text-orange-300 transition-all duration-200"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           {menuOpen ? <X className="w-7 h-7" /> : <Menu className="w-7 h-7" />}
@@ -52,11 +52,14 @@ export default function Header() {
       </div>
 
       {menuOpen && (
-        <div className="medium:hidden border-t border-green-700">
+        <div className="lg:hidden border-t border-green-700">
           <nav className="flex flex-col p-4 space-y-4">
             <Link to="/" onClick={() => {window.scrollTo({ top: 0, behavior: "smooth" });setMenuOpen(false);}} className={`flex items-center gap-2 ${isActive("/") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} font-semibold`}>
               <Home className="w-5 h-5" /> Home
             </Link>
+            <Link to="/sobre" onClick={() => {window.scrollTo({ top: 0, behavior: "smooth" });setMenuOpen(false);}} className={`flex items-center gap-2 ${isActive("/sobre") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} font-semibold`}>
+              <BookOpen className="w-5 h-5" /> Sobre Nós
+            </Link>            
             <Link to="/produtos" onClick={() => setMenuOpen(false)} className={`flex items-center gap-2 ${isActive("/produtos") ? "text-orange-300" : "text-orange-100 hover:text-orange-300"} font-semibold`}>
               <Boxes className="w-5 h-5" /> Produtos
             </Link>
