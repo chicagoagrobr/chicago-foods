fa2e3ee1f47cea2204fb9d5329344f4790769767
sophiasussa <sophiasussa@outlook.com>
2025-10-21 14:17:31 -0300

    style: add privacypolicy page dark mode colors

--- src/pages/PrivacyPolicyContent.jsx
+++ src/pages/PrivacyPolicyContent.jsx
@@ -15,33 +15,33 @@ export default function PrivacyPolicyContent() {
         image="https://chicagofoods.com.br/cf_512_square_adjusted.png"
       />
 
-      <section className="min-h-screen text-gray-800">
+      <section className="min-h-screen text-gray-800 dark:bg-zinc-900 dark:text-gray-200">
         <div className="relative flex items-center justify-center h-64">
           <div className="text-center px-6">
             <h1 className="text-lg sm:text-3xl mb-2 text-green-800 font-bold">
               {t("header.title")}
             </h1>
-            <p className="max-w-2xl text-gray-700 mx-auto text-sm sm:text-lg">
+            <p className="max-w-2xl text-gray-700 dark:text-gray-200 mx-auto text-sm sm:text-lg">
               {t("header.intro")}
             </p>
           </div>
         </div>
 
-        <div className="max-w-4xl mx-auto mb-12 px-6 space-y-12">
+        <div className="max-w-4xl mx-auto pb-12 px-6 space-y-12">
           {sections.map((section, idx) => (
             <div
               key={idx}
-              className="bg-white shadow-2xl hover:scale-105 transition rounded-lg p-8"
+              className="bg-white dark:bg-zinc-700 shadow-2xl hover:scale-105 transition rounded-lg p-8"
             >
               <h2 className="text-2xl font-semibold mb-4">{section.title}</h2>
               {Array.isArray(section.content) ? (
-                <ul className="list-disc list-inside space-y-2 text-gray-700">
+                <ul className="list-disc list-inside space-y-2 text-gray-700 dark:text-gray-100">
                   {section.content.map((item, i) => (
                     <li key={i}>{item}</li>
                   ))}
                 </ul>
               ) : (
-                <p className="text-gray-700">{section.content}</p>
+                <p className="text-gray-700 dark:text-gray-100">{section.content}</p>
               )}
             </div>
           ))}
